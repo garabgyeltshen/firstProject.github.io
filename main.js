4a955c2aae54cea64048ecb7d34db14e89a914d1
garabgyeltshen <70823082+garabgyeltshen@users.noreply.github.com>
2020-10-07 19:54:06 +0530

    my first project

---
+++ main.js
@@ -0,0 +1,9 @@
+const tipcalcy = () => {
+    let amount = document.getElementById("bill_amount").value;
+    let perc = document.getElementById("tip_perc").value;
+    let tip = amount * (perc / 100);
+
+    let total = tip + Number(amount);
+    document.getElementById('tiP_total').value = tip;
+    document.getElementById('total_billed').value = total;
+}
